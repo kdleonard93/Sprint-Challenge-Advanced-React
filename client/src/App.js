27ce1b9fd37884e835fcdf2a575b2f74542f7879
Kyle Leonard <kdleonard@icloud.com>
2019-12-15 19:33:13 -0600

    Got all data to display

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import PlayerCard from "./components/PlayerCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      players: []
+    };
+  }
+
+  // const [players, setPlayers]=useState([]);
+
+  componentDidMount() {
+    fetch(`http://localhost:5000/api/players`)
+      .then(res => res.json())
+      .then(res => this.setState({ players: res }))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    console.log(this.state.players);
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Women's World Cup Soccer</h1>
+        </header>
+        <PlayerCard players={this.state.players} />
+      </div>
+    );
+  }
 }
-
 export default App;
